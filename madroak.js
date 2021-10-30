0f4fee01961d60bd559653b58785a3b654cb506e
dante01yoon <bunggl@naver.com>
2021-10-30 20:00:03 +0900

    Update: 정적파일 미들웨어

--- madroak.js
+++ madroak.js
@@ -3,14 +3,14 @@ const express = require("express");
 const expressHandlebars = require("express-handlebars");
 const app = express();
 const port = process.env.PORT || 3000;
-
+// 정적 파일 세팅
+app.use(express.static(__dirname + "/public"));
 // 핸들바 뷰 엔진 설정
 app.engine(".hbs", expressHandlebars({
   defaultLayout: "main",
   extname: ".hbs",
 }))
 app.set("view engine", ".hbs");
-
 
 app.get("/", (req, res) => res.render("home"));
 
