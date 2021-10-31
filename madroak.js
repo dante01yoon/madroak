717c50227d9a584545efa7eefef0328f5b208b86
dante01yoon <bunggl@naver.com>
2021-10-31 12:33:11 +0900

    Update: handler test code 작성

--- madroak.js
+++ madroak.js
@@ -1,6 +1,7 @@
 const { errorLog, appLog } = require("./util");
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
+const handlers = require("./lib/handlers");
 const app = express();
 const port = process.env.PORT || 3000;
 // 정적 파일 세팅
@@ -12,36 +13,15 @@ app.engine(".hbs", expressHandlebars({
 }))
 app.set("view engine", ".hbs");
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) => handlers.home);
 
-app.get("/about*", (req, res) => {
-  const fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-  ]
-
-  res.render("about", {
-    fortune: fortunes[~~(Math.random() * fortunes.length)],
-  });
-})
+app.get("/about*", (req, res) => handlers.about)
 
 // 404
-app.use((req, res) => {
-  res.type("text/plain");
-  res.status(404);
-  res.send("404 - Not Found");
-})
+app.use((req, res) => handlers.notFound)
 
 // 500
-app.use((err, req, res, next) => {
-  errorLog(err.message);
-  res.type("text/plain");
-  res.status(500);
-  res.send("500 - Server Error");
-})
+app.use((err, req, res, next) => handlers.serverError)
 
 app.listen(port, (req) => {
   appLog(`Express started on port ${port}`)
